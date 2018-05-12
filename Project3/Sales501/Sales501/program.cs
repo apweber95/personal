464ab419f55e68d0a70e40dabe38cc7351f31976
Andrew Weber <apweber@ksu.edu>
2018-05-12 16:15:42 -0500

    added projects

---
+++ Project3/Sales501/Sales501/program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales501
+{
+    class program
+    {
+        private static void Main(string[] args)
+        {
+            UI ui = new UI();
+            ui.run();
+        }
+    }
+}
